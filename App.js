9ba80f5dfa2af11aed199de5ea7612ddee9f1247
Anurag <anuragarwalkar@gmail.com>
2021-03-11 14:03:29 +0530

    removed used code

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Demo Works</Text>
+      <Text>React Native Works</Text>
     </View>
   );
 };
